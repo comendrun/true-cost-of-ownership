766d2bd411309cbc9249a045face5c2607be4bed
comendrun <kamran.rouhani@outlook.com>
2025-01-26 11:51:09 +0100

    refactor: Enhance onNextStep function to utilize advancedFormSteps for dynamic step validation

--- app/dashboard/add-car/_components/helper-functions.ts
+++ app/dashboard/add-car/_components/helper-functions.ts
@@ -1,16 +1,15 @@
 import { formStepIdMap } from '../_consts/consts'
-import { FormStepsIDs } from '../_types/types'
+import { CarFormFields, FormSteps, FormStepsIDs } from '../_types/types'
 
 export function onNextStep(
   currentStep: string,
   currentIndex: number,
-  setState: React.Dispatch<React.SetStateAction<FormStepsIDs>>
+  setState: React.Dispatch<React.SetStateAction<FormStepsIDs>>,
+  advancedFormSteps: FormSteps<CarFormFields>[]
 ) {
-  console.log(`Current Step: ${currentStep}, Current Index: ${currentIndex}`)
-  if (currentIndex < 0 || currentIndex >= 11) return
+  if (currentIndex < 0 || currentIndex >= advancedFormSteps.length - 1) return
   const nextStepIndex = currentIndex + 1
   const nextStepID: FormStepsIDs = formStepIdMap[nextStepIndex]
-  console.log(`Next Step Index: ${nextStepIndex}, Next Step ID: ${nextStepID}`)
   return setState(nextStepID)
 }
 
@@ -19,12 +18,8 @@ export function onPreviousStep(
   currentIndex: number,
   setState: React.Dispatch<React.SetStateAction<FormStepsIDs>>
 ) {
-  console.log(`Current Step: ${currentStep}, Current Index: ${currentIndex}`)
   if (currentIndex === 0) return
   const previousStepIndex = currentIndex - 1
   const previousStepID: FormStepsIDs = formStepIdMap[previousStepIndex]
-  console.log(
-    `Previous Step Index: ${previousStepIndex}, Previous Step ID: ${previousStepID}`
-  )
   return setState(previousStepID)
 }
