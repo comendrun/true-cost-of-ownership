3d0d78021a085294008e67c4f764e940144d3606
comendrun <kamran.rouhani@outlook.com>
2025-02-23 14:05:32 +0100

    feat: Update theme toggle button style to use ghost variant for better visibility

--- src/components/ui/providers/theme-toggle.tsx
+++ src/components/ui/providers/theme-toggle.tsx
@@ -37,7 +37,7 @@ export function ModeToggle() {
               <Avatar className='h-8 w-8 rounded-lg'>
                 {/* <AvatarImage src={'avatarUrl'} alt={'Theme Mode'} /> */}
                 <AvatarFallback className='rounded-lg'>
-                  <Button variant='outline' size='icon'>
+                  <Button variant='ghost' size='icon' className='bg-transparent'>
                     <Sun className='rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
                     <Moon className='absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
                     <span className='sr-only'>Toggle theme</span>
