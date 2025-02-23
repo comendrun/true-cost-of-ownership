bbbf5460b1c2ad517ade28e75bc10fc37809f55f
comendrun <kamran.rouhani@outlook.com>
2025-02-23 13:46:02 +0100

    feat: Integrate UserAnalysisOverview component into dashboard page for enhanced user insights

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,6 @@
 'use server'
 
+import UserAnalysisOverview from '@/components/pages/dashboard/components/user-analysis-overview'
 import UserCarsOverview from '@/components/pages/dashboard/components/user-cars-overview'
 import ErrorPage from '@/components/ui/page-components/error-page-component'
 import { createClient } from '@/utils/supabase/server'
@@ -21,22 +22,28 @@ export default async function Page() {
     .select('*')
     .eq('user_id', user.id)
 
-  if (!userCars || getUserCarsError) {
-    return (
-      <ErrorPage description='There was an error while trying to get the User cars' />
-    )
-  }
+  const userCarsIds = userCars?.reduce((acc, car) => {
+    acc.push(car?.id)
+    return acc
+  }, [] as number[])
+
+  const { data: userAnalysis, error: getUserAnalysisError } = await supabase
+    .from('ai_responses')
+    .select('*')
+    .eq('car_id', userCarsIds || [])
 
   return (
     <>
-      <div className='grid auto-rows-min gap-4 md:grid-cols-2'>
+      <div className='grid auto-rows-min gap-4 xl:grid-cols-2'>
         {/* <div className='aspect-video rounded-xl bg-muted/50' /> */}
         <div className='aspect-video rounded-xl bg-muted/50 p-5'>
           <UserCarsOverview userCars={userCars} />
         </div>
-        <div className='aspect-video rounded-xl bg-muted/50' />
+        <div className='aspect-video rounded-xl bg-muted/50 p-5'>
+          <UserAnalysisOverview userAnalysisList={userAnalysis} />
+        </div>
       </div>
-      <div className='min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min' />
+      {/* <div className='min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min' /> */}
     </>
   )
 }
