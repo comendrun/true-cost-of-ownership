38db3d216c67feb4f0f88b75321d77fe623ad1c7
comendrun <kamran.rouhani@outlook.com>
2025-02-24 01:16:28 +0100

    feat: Enhance dashboard page with loading skeleton and update AI responses fetching

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,7 +3,9 @@
 import UserAnalysisOverview from '@/components/pages/dashboard/components/user-analysis-overview'
 import UserCarsOverview from '@/components/pages/dashboard/components/user-cars-overview'
 import ErrorPage from '@/components/ui/page-components/error-page-component'
+import { Skeleton } from '@/components/ui/skeleton'
 import { createClient } from '@/utils/supabase/server'
+import { Suspense } from 'react'
 
 export default async function Page() {
   const supabase = createClient()
@@ -21,6 +23,7 @@ export default async function Page() {
     .from('user_cars')
     .select('*')
     .eq('user_id', user.id)
+    .order('created_at', { ascending: false })
 
   const userCarsIds = userCars?.reduce((acc, car) => {
     acc.push(car?.id)
@@ -28,19 +31,31 @@ export default async function Page() {
   }, [] as number[])
 
   const { data: userAnalysis, error: getUserAnalysisError } = await supabase
-    .from('ai_responses')
-    .select('*')
-    .eq('car_id', userCarsIds || [])
+    // .from('ai_responses')
+    // .select('*')
+    // .in('car_id', userCarsIds || [])
+    // .order('created_at', { ascending: false })
+    .rpc('get_latest_ai_responses', { car_ids: userCarsIds || [] })
 
   return (
     <>
       <div className='grid auto-rows-min gap-4 xl:grid-cols-2'>
         {/* <div className='aspect-video rounded-xl bg-muted/50' /> */}
+
+        <Suspense
+          fallback={
+            <Skeleton className='aspect-video rounded-xl bg-muted/50 p-5' />
+          }
+        >
+          <div className='aspect-video rounded-xl bg-muted/50 p-5'>
+            <UserCarsOverview userCars={userCars} />
+          </div>
+        </Suspense>
         <div className='aspect-video rounded-xl bg-muted/50 p-5'>
-          <UserCarsOverview userCars={userCars} />
-        </div>
-        <div className='aspect-video rounded-xl bg-muted/50 p-5'>
-          <UserAnalysisOverview userAnalysisList={userAnalysis} />
+          <UserAnalysisOverview
+            userAnalysisList={userAnalysis}
+            userCars={userCars}
+          />
         </div>
       </div>
       {/* <div className='min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min' /> */}
