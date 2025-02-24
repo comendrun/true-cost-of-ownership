3511e6c3dbd728d4364ae77b062595b7d1faae8f
comendrun <kamran.rouhani@outlook.com>
2025-02-24 01:16:53 +0100

    feat: Add table to display user cars in UserCarsOverview component

--- src/components/pages/dashboard/components/user-cars-overview.tsx
+++ src/components/pages/dashboard/components/user-cars-overview.tsx
@@ -1,6 +1,15 @@
 'use client'
 import { UserCarsTableRow } from '@/components/types/add-car/types'
 import { Button } from '@/components/ui/button'
+import { Card, CardHeader, CardTitle } from '@/components/ui/card'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow
+} from '@/components/ui/table'
 import Link from 'next/link'
 
 export default function UserCarsOverview({
@@ -10,19 +19,50 @@ export default function UserCarsOverview({
 }) {
   return (
     <div className='flex h-full w-full flex-col justify-between gap-5'>
-      <div className='m-auto flex h-full w-full max-w-[300px] flex-col justify-center'>
+      <div className='flex h-max w-full flex-row items-center justify-start gap-2'>
         {userCars ? (
           <>
             <p>You have saved</p>
-            <p className='mx-auto text-[50px] font-bold md:text-[100px]'>
+            <p className='text-[50px] font-bold md:text-xl'>
               {userCars.length}
             </p>
-            <p className='ml-auto'>Cars in total.</p>
+            <p className=''>Cars in total.</p>
           </>
         ) : (
           <p>You added no cars yet.</p>
         )}
       </div>
+
+      {userCars && (
+        // <Card>
+        //   <CardHeader>
+        //     <CardTitle>Your Saved Cars</CardTitle>
+        //   </CardHeader>
+
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Name</TableHead>
+              <TableHead>Year</TableHead>
+              <TableHead>TCO</TableHead>
+              <TableHead>Link</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {userCars.slice(0, 5).map((car, index) => (
+              <TableRow>
+                <TableCell>{car.name}</TableCell>
+                <TableCell>{car.year}</TableCell>
+                <TableCell>{car.tco}</TableCell>
+                <TableCell>
+                  <Link href={`/dashboard/my-cars/${car.id}`}>Details</Link>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        // </Card>
+      )}
 
       <div className='flex w-full gap-4'>
         <Button asChild variant='secondary' className='ml-auto'>
