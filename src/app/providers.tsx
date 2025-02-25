39cf29abaed17d39f7fe09752cb85a726d69342a
comendrun <kamran.rouhani@outlook.com>
2025-02-25 08:43:49 +0100

    refactor: Rename Providers to ClientProviders and add user prop for user profile handling

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,8 +1,16 @@
+'use client'
+import { UserProfile } from '@/components/types/add-car/types'
 import { ThemeProvider } from '@/components/ui/providers/theme-provider'
 import { Toaster } from '@/components/ui/sonner'
 import { ReactNode } from 'react'
 
-export default function Providers({ children }: { children: ReactNode }) {
+export default function ClientProviders({
+  children,
+  user
+}: {
+  children: ReactNode
+  user: UserProfile | null
+}) {
   return (
     <>
       <ThemeProvider
