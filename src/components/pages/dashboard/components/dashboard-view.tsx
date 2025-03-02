00eaa4dd05d0369c8d42c6ac9b8356e885396915
comendrun <kamran.rouhani@outlook.com>
2025-03-02 04:06:09 +0100

    feat: Add DashboardView component to display user cars and analysis overview

---
+++ src/components/pages/dashboard/components/dashboard-view.tsx
@@ -0,0 +1,21 @@
+'use client'
+import React from 'react'
+import UserAnalysisOverview from './user-analysis-overview'
+import UserCarsOverview from './user-cars-overview'
+import useGetUserCars from '@/hooks/use-get-user-cars'
+
+export default function DashboardView() {
+  const { cars, isLoading } = useGetUserCars()
+
+  const userCarsWithAnalysis = cars?.filter(car => car.last_ai_response_id)
+
+  return (
+    <>
+      <UserCarsOverview userCars={cars} isLoading={isLoading} />
+      <UserAnalysisOverview
+        userCarsWithAnalysis={userCarsWithAnalysis}
+        isLoading={isLoading}
+      />
+    </>
+  )
+}
