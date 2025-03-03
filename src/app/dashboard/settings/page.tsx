321dd408a425041e928dc9d965dd666215165410
comendrun <kamran.rouhani@outlook.com>
2025-03-03 01:56:27 +0100

    feat: Refactor SettingsPage layout for improved styling and structure

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -5,5 +5,12 @@ import React from 'react'
 export default function SettingsPage() {
   // return <ComingSoon />
 
-  return <UserSettings />
+  return (
+    <div className='min-h-max flex-1 rounded-xl bg-muted/50 p-10'>
+      <div className='flex flex-col gap-3 max-w-[500px]'>
+
+      <UserSettings />
+      </div>
+    </div>
+  )
 }
