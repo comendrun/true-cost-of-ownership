1807b6ac9f10ad60233b264e2901bc255600aec5
comendrun <kamran.rouhani@outlook.com>
2025-03-08 02:43:38 +0100

    feat: Increase max width of settings page layout for improved user experience

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -1,13 +1,11 @@
 import UserSettings from '@/components/pages/dashboard/settings/user-settings'
-import ComingSoon from '@/components/ui/page-components/coming-soon'
-import React from 'react'
 
 export default function SettingsPage() {
   // return <ComingSoon />
 
   return (
     <div className='min-h-max flex-1 rounded-xl bg-muted/50 p-10'>
-      <div className='flex max-w-[500px] flex-col gap-3'>
+      <div className='flex max-w-[600px] flex-col gap-3'>
         <UserSettings />
       </div>
     </div>
