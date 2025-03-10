a4877b2ecc85cd865e67a968305134808478ef07
comendrun <kamran.rouhani@outlook.com>
2025-03-10 09:27:17 +0100

    feat: Enhance user store persistence with JSON storage and improve clearStorage logic

--- src/lib/users.store.ts
+++ src/lib/users.store.ts
@@ -2,7 +2,7 @@
 import { UserProfile } from '@/components/types/add-car/types'
 import { LOCAL_STORAGE_KEYS } from '@/consts/app-constants'
 import { create } from 'zustand'
-import { persist } from 'zustand/middleware'
+import { persist, createJSONStorage } from 'zustand/middleware'
 
 export type State = {
   user: UserProfile | null
@@ -13,25 +13,30 @@ export type Actions = {
   clearStorage: () => void
 }
 
-const isBrowser = typeof window !== 'undefined'
-
-const localStoredUser = isBrowser
-  ? (JSON.parse(
-      localStorage.getItem(LOCAL_STORAGE_KEYS.USER_INFO_LOCAL_STORAGE_KEY) ||
-        '{}'
-    )?.state as State)
-  : null
-
 export const useUserStore = create<State & Actions>()(
   persist(
     set => ({
-      user: localStoredUser?.user || null,
+      user: null,
       setUser: (values: UserProfile | null) => set(() => ({ user: values })),
-      clearStorage: () =>
-        localStorage.removeItem(LOCAL_STORAGE_KEYS.USER_INFO_LOCAL_STORAGE_KEY)
+      clearStorage: () => {
+        if (typeof window !== 'undefined') {
+          localStorage.removeItem(
+            LOCAL_STORAGE_KEYS.USER_INFO_LOCAL_STORAGE_KEY
+          )
+        }
+      }
     }),
     {
       name: LOCAL_STORAGE_KEYS.USER_INFO_LOCAL_STORAGE_KEY,
+      storage: createJSONStorage(() =>
+        typeof window !== 'undefined'
+          ? localStorage
+          : {
+              getItem: () => null,
+              setItem: () => {},
+              removeItem: () => {}
+            }
+      ),
       partialize: state => ({
         user: state?.user
       })
