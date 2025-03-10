6213de6d419f86d063a1c7e5da64743bc9989001
comendrun <kamran.rouhani@outlook.com>
2025-03-11 00:47:13 +0100

    refactor: Remove unused AI analysis generation function from car form component

--- src/components/pages/dashboard/add-car/components/car-form.tsx
+++ src/components/pages/dashboard/add-car/components/car-form.tsx
@@ -139,32 +139,6 @@ export default function CarForm({
 
   const { errors, isLoading } = formState
 
-  // async function handleGenerateAIAnalysis(
-  //   id: string | number | null,
-  //   setIsAnalysisGenerating: Dispatch<SetStateAction<boolean>>
-  // ): Promise<void> {
-  //   if (id) {
-  //     setIsAnalysisGenerating(true)
-  //     try {
-  //       const result = await openaiCostsAnalysisCompletion({
-  //         userCarId: id
-  //       })
-  //       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  //     } catch (error: any) {
-  //       console.error(
-  //         'There was an error while generating the AI Analysis:',
-  //         error?.message
-  //       )
-  //       toast.error(
-  //         error.message ||
-  //           'There was an error while trying to generate the car analysis.'
-  //       )
-  //     } finally {
-  //       setIsAnalysisGenerating(false)
-  //     }
-  //   }
-  // }
-
   const countryField = formFields['country'] as SelectField<CarFormFields>
 
   return (
