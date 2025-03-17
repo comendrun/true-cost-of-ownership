a6e32ee3eedd4d79a9dd2aa9b311950ceed11074
comendrun <kamran.rouhani@outlook.com>
2025-03-17 11:14:13 +0100

    feat: Refactor CarsPage layout for improved styling and structure

--- src/app/dashboard/add-car/simple/page.tsx
+++ src/app/dashboard/add-car/simple/page.tsx
@@ -3,9 +3,11 @@ import React from 'react'
 
 export default function CarsPage() {
   return (
-    <div className='flex flex-col items-center justify-center gap-5 lg:grid lg:grid-cols-3'>
-      <div className='col-span-2'></div>
-      <div className='col-span-1 flex h-full w-full'>Simple Form</div>
-    </div>
+    // <div className='flex flex-col items-center justify-center gap-5 lg:grid lg:grid-cols-3'>
+    //   <div className='col-span-2'></div>
+    //   <div className='col-span-1 flex h-full w-full'>Simple Form</div>
+    // </div>
+    // <div className='flex flex-col items-center justify-center gap-5'>Simple Form</div>
+    <div className='min-h-max flex-1 rounded-xl bg-muted/50 p-5 max-w-full'>Simple</div>
   )
 }
