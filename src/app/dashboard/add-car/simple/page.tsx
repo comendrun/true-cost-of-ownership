e07567cb3e861e9f81b2d8153baedd3dd64f7215
comendrun <kamran.rouhani@outlook.com>
2025-03-17 11:14:18 +0100

    feat: Integrate SimpleForm component into CarsPage for enhanced user interaction

--- src/app/dashboard/add-car/simple/page.tsx
+++ src/app/dashboard/add-car/simple/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import SimpleForm from '@/components/pages/dashboard/add-car/components/simple-form'
 import React from 'react'
 
 export default function CarsPage() {
@@ -8,6 +9,10 @@ export default function CarsPage() {
     //   <div className='col-span-1 flex h-full w-full'>Simple Form</div>
     // </div>
     // <div className='flex flex-col items-center justify-center gap-5'>Simple Form</div>
-    <div className='min-h-max flex-1 rounded-xl bg-muted/50 p-5 max-w-full'>Simple</div>
+    <div className='min-h-max max-w-full flex-1 rounded-xl bg-muted/50 p-5'>
+      {/* a navigation button to go through the questions */}
+
+      <SimpleForm />
+    </div>
   )
 }
