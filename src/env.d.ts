08c6aae9b5478bad078941977415e4815c38a6e4
comendrun <kamran.rouhani@outlook.com>
2025-03-21 02:12:18 +0100

    refactor: Remove unused authentication components and environment type definitions

--- src/env.d.ts
+++
@@ -1,9 +0,0 @@
-namespace NodeJS {
-  interface ProcessEnv {
-    NEXT_PUBLIC_SUPABASE_URL: string
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: string
-    SUPBASE_PROJECT_REFERENCE_ID: string
-    OPENAI_API_KEY: string
-    NEXT_PUBLIC_HOST: string
-  }
-}
