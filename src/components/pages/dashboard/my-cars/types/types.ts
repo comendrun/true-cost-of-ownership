03cf0a6b007bc5fea620dae3c498bf309b0adf28
comendrun <kamran.rouhani@outlook.com>
2025-03-22 00:05:17 +0100

    refactor: Remove unused components and types related to my-cars feature

--- src/components/pages/dashboard/my-cars/types/types.ts
+++
@@ -1,7 +0,0 @@
-import { UserCarsTableRow } from '../../../../../features/add-car/types/add-cars.types'
-
-export type CarsDataTableProps = {
-  userCarsCount: number | null
-  initialData: UserCarsTableRow[] | null
-  error: Error | null
-}
