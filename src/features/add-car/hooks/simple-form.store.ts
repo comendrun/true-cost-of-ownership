70633d9c09781533f4268c8952982218f04d4420
comendrun <kamran.rouhani@outlook.com>
2025-03-24 00:59:16 +0100

    feat: Add clearData method and switch storage to sessionStorage in SimpleForm store

--- src/features/add-car/hooks/simple-form.store.ts
+++ src/features/add-car/hooks/simple-form.store.ts
@@ -8,11 +8,12 @@ type SimpleFormState = {
 
 type SimpleFormAction = {
   setData: (data: Partial<SimpleFormFields>) => void
+  clearData: () => void // Method to manually clear the storage
 }
 
 export const useSimpleFormDataStore = create<
   SimpleFormState & SimpleFormAction
->(
+>()(
   persist(
     set => ({
       data: {},
@@ -22,11 +23,15 @@ export const useSimpleFormDataStore = create<
             ...prevValues.data,
             ...data
           }
-        }))
+        })),
+      clearData: () => set({ data: {} }) // Clear the in-memory state
     }),
     {
       name: 'simple-form-data',
-      storage: createJSONStorage(() => localStorage)
+      storage: createJSONStorage(() => sessionStorage), // Use session storage
+      partialize: state => ({
+        data: state.data
+      })
     }
   )
 )
