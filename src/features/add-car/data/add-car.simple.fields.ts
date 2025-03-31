435f6336be1578ee93510b4bb5cf246405a4b5d5
comendrun <kamran.rouhani@outlook.com>
2025-04-01 00:44:48 +0200

    feat: Add simple form fields for car, driver, and financial information

---
+++ src/features/add-car/data/add-car.simple.fields.ts
@@ -0,0 +1,38 @@
+import { FormFieldType } from '@/types/form-field.types'
+import {
+  SimpleFormCarInfoType,
+  SimpleFormDriverInfo,
+  SimpleFormFinancialInfo
+} from '../types/add-car.simple.types'
+import { getFormField } from './fields'
+
+export const simpleFormCarInfoFields: FormFieldType<SimpleFormCarInfoType>[] = [
+  getFormField('brand', { fullWidth: true }), // Required
+  getFormField('model'), // Required
+  getFormField('year'), // Required
+  getFormField('mileage'), // Required
+  getFormField('fuelType') // Required
+]
+
+export const simpleFormFinancialInfoFields: FormFieldType<SimpleFormFinancialInfo>[] =
+  [
+    getFormField('purchasePrice'),
+    getFormField('prepayment'),
+    getFormField('plannedYearsOfOwnership')
+  ]
+
+export const simpleFormDriverInfoFields: FormFieldType<SimpleFormDriverInfo>[] =
+  [
+    getFormField('driverAgeRange'),
+    getFormField('drivingExperienceYears'),
+    getFormField('country')
+  ]
+
+export const simpleFormCarInfoFieldsKeys =
+  simpleFormCarInfoFields.map(field => field.key)
+
+export const simpleFormFinancialInfoFieldsKeys: string[] =
+  simpleFormFinancialInfoFields.map(field => field.key)
+
+export const simpleFormDriverInfoFieldsKeys: string[] =
+  simpleFormDriverInfoFields.map(field => field.key)
