2bedca4ede30dc930b9f164bfc7fbf6c7ad9550f
comendrun <kamran.rouhani@outlook.com>
2025-04-25 10:25:13 +0200

    feat: add loading state and error handling to driver info submission

--- src/features/add-car/components/simple-form/driver-info/driver-info-step.tsx
+++ src/features/add-car/components/simple-form/driver-info/driver-info-step.tsx
@@ -10,12 +10,14 @@ import {
   FormLabel,
   FormMessage
 } from '@/components/ui/form'
+import { LoadingDialogWithSpinner } from '@/components/ui/loading/LoadingDialogWithSpinner'
 import {
   simpleFormCarInfoFieldsKeys,
   simpleFormDriverInfoFields,
   simpleFormFinancialInfoFieldsKeys
 } from '@/features/add-car/data/add-car.simple.fields'
 import { useSimpleFormDataStore } from '@/features/add-car/hooks/simple-form.store'
+import { submitSimpleFormData } from '@/features/add-car/server/actions/submit-simple-form.server.action'
 import {
   SimpleFormDriverInfoSchema,
   SimpleFormFieldsSchema
@@ -23,16 +25,14 @@ import {
 import {
   SimpleFormCarInfoType,
   SimpleFormDriverInfo,
-  SimpleFormFields,
   SimpleFormFinancialInfo
 } from '@/features/add-car/types/add-car.simple.types'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useRouter } from 'next/navigation'
 import { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
-import SimpleFormStepCard from '../SimpleFormStepCard'
-import { submitSimpleFormData } from '@/features/add-car/server/actions/submit-simple-form.server.action'
 import { toast } from 'sonner'
+import SimpleFormStepCard from '../SimpleFormStepCard'
 
 export default function SimpleFormDriverInfoStep() {
   const router = useRouter()
@@ -50,18 +50,12 @@ export default function SimpleFormDriverInfoStep() {
   })
 
   const onSubmit = async (data: SimpleFormDriverInfo) => {
-    console.log('submitted data', { ...formData, ...data })
     setData(data)
 
     // const finalData: SimpleFormFields = {...formData, ...data}
     // console.log('finalData', finalData)
 
-    console.log('data', data)
-    console.log('formData', formData)
-
     const parsedData = SimpleFormFieldsSchema.safeParse(formData)
-
-    console.log('parsed', parsedData)
 
     if (!parsedData.success) {
       toast.error(
@@ -70,14 +64,22 @@ export default function SimpleFormDriverInfoStep() {
       return
     }
 
-    const response = submitSimpleFormData(parsedData?.data)
-
-    // clearData()
+    try {
+      const response = await submitSimpleFormData(parsedData?.data)
+      console.log('response', response)
+      router.push(`/dashboard/my-cars/${response.id}`) // PROBLEM: The page is not shown even though the vehicle exists.
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      console.error('error', error?.message)
+      toast.error(
+        `There was an error while trying to add your new Car to our System. Please try again after a while.`
+      )
+    }
   }
 
   const {
     control,
-    formState: { errors, isLoading },
+    formState: { errors, isLoading, isSubmitting },
     watch,
     reset
   } = form
@@ -110,87 +112,100 @@ export default function SimpleFormDriverInfoStep() {
     }
   }, [formData, reset])
 
+  if (isSubmitting) {
+    return (
+      <LoadingDialogWithSpinner
+        open={true}
+        title='Please wait...'
+        description='Please wait until your car is added.'
+        withCloseButton={false}
+      />
+    )
+  }
+
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className=''>
-        <SimpleFormStepCard
-          cardTitle='Driver Information'
-          cardDescription='Add a new Vehicle to Monitor in three simple steps.'
-          cardFooter={
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className=''>
+          <SimpleFormStepCard
+            cardTitle='Driver Information'
+            cardDescription='Add a new Vehicle to Monitor in three simple steps.'
+            cardFooter={
+              <>
+                <Button
+                  type='button'
+                  className=''
+                  variant='outline'
+                  onClick={onPreviousSimpleFormStep}
+                >
+                  previous Step
+                </Button>
+                <Button type='submit' className='ml-auto'>
+                  Submit
+                </Button>
+              </>
+            }
+          >
             <>
-              <Button
-                type='button'
-                className=''
-                variant='outline'
-                onClick={onPreviousSimpleFormStep}
-              >
-                previous Step
-              </Button>
-              <Button type='submit' className='ml-auto'>
-                Submit
-              </Button>
-            </>
-          }
-        >
-          <>
-            {simpleFormDriverInfoFields.map((formField, index) => {
-              const {
-                formDescription,
-                component,
-                key,
-                label,
-                required,
-                type,
-                disabled,
-                fullWidth,
-                infoField,
-                placeholder
-              } = formField
+              {simpleFormDriverInfoFields.map((formField, index) => {
+                const {
+                  formDescription,
+                  component,
+                  key,
+                  label,
+                  required,
+                  type,
+                  disabled,
+                  fullWidth,
+                  infoField,
+                  placeholder
+                } = formField
 
-              return (
-                <FormField
-                  key={`simple-form-${index}-${formField.key}`}
-                  control={control}
-                  name={key}
-                  render={({ field }) => {
-                    const value =
-                      typeof field.value === 'boolean' ? '' : field.value
-                    const errorMessage = errors?.[key]?.message as
-                      | string
-                      | undefined
-                    return (
-                      <FormItem
-                        className={`grid w-full grid-cols-4 items-center ${fullWidth ? 'col-span-2' : 'col-span-1'}`}
-                      >
-                        {/* <div className='grid grid-cols-4 items-center gap-4'> */}
-
-                        <FormLabel
-                          className={`${required && !disabled ? 'required-field' : ''} col-span-2`}
+                return (
+                  <FormField
+                    key={`simple-form-${index}-${formField.key}`}
+                    control={control}
+                    name={key}
+                    render={({ field }) => {
+                      const value =
+                        typeof field.value === 'boolean' ? '' : field.value
+                      const errorMessage = errors?.[key]?.message as
+                        | string
+                        | undefined
+                      return (
+                        <FormItem
+                          className={`grid w-full grid-cols-4 items-center ${fullWidth ? 'col-span-2' : 'col-span-1'}`}
                         >
-                          {label}
-                        </FormLabel>
-                        <FormControl className=''>
-                          <div className='col-span-2'>
-                            <DynamicFormFieldInput<SimpleFormDriverInfo>
-                              errors={errors}
-                              control={control}
-                              formField={formField}
-                              field={field}
-                              watch={watch}
-                            />
-                          </div>
-                        </FormControl>
-                        <FormDescription>{formDescription}</FormDescription>
-                        <FormMessage>{errorMessage}</FormMessage>
-                      </FormItem>
-                    )
-                  }}
-                />
-              )
-            })}
-          </>
-        </SimpleFormStepCard>
-      </form>
-    </Form>
+                          {/* <div className='grid grid-cols-4 items-center gap-4'> */}
+
+                          <FormLabel
+                            className={`${required && !disabled ? 'required-field' : ''} col-span-2`}
+                          >
+                            {label}
+                          </FormLabel>
+                          <FormControl className=''>
+                            <div className='col-span-2'>
+                              <DynamicFormFieldInput<SimpleFormDriverInfo>
+                                errors={errors}
+                                control={control}
+                                formField={formField}
+                                field={field}
+                                watch={watch}
+                              />
+                            </div>
+                          </FormControl>
+                          <FormDescription>{formDescription}</FormDescription>
+                          <FormMessage>{errorMessage}</FormMessage>
+                        </FormItem>
+                      )
+                    }}
+                  />
+                )
+              })}
+            </>
+          </SimpleFormStepCard>
+        </form>
+      </Form>
+    </>
   )
 }
