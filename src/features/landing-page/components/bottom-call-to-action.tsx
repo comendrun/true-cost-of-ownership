64c4efb581520654c5f0014f73876cb3b3147909
comendrun <kamran.rouhani@outlook.com>
2025-05-04 18:41:12 +0200

    adding more margin to the top of the link preview to make it more readable

--- src/features/landing-page/components/bottom-call-to-action.tsx
+++ src/features/landing-page/components/bottom-call-to-action.tsx
@@ -7,21 +7,27 @@ import Link from 'next/link'
 
 const BottomCallToActionSection = () => {
   return (
-    <LandingPageSection className="flex justify-between items-center gap-2 px-5 max-w-3xl w-full m-5 min-h-[20vh]">
-      <p className="text-lg md:text-2xl flex gap-3 flex-col md:flex-row md:items-center">
-        <span className="">AutoMon</span> <span className="">- by <LinkPreview url="https://comendrun.com"
-                                                                               className="font-semibold">comendrun</LinkPreview></span>
+    <LandingPageSection className='m-5 my-10 mt-20 flex min-h-[20vh] w-full max-w-3xl items-center justify-between gap-2 px-5'>
+      <p className='flex flex-col gap-3 text-lg md:flex-row md:items-center md:text-2xl'>
+        <span className=''>AutoMon</span>{' '}
+        <span className=''>
+          - by{' '}
+          <LinkPreview url='https://comendrun.com' className='font-semibold'>
+            comendrun
+          </LinkPreview>
+        </span>
       </p>
-      <div className="flex gap-2 flex-col md:flex-row items-center justify-center ">
-        <Button className="w-full text-xs md:text-md " asChild>
-          <Link href="/dashboard">
-            Get Started
-          </Link>
+      <div className='flex flex-col items-center justify-center gap-2 md:flex-row'>
+        <Button className='md:text-md w-full text-xs' asChild>
+          <Link href='/dashboard'>Get Started</Link>
         </Button>
-        <Button variant="outline" size="default" className="w-full h-full text-xs md:text-md" asChild>
-          <a href="https://comendrun.com">
-            My Website
-          </a>
+        <Button
+          variant='outline'
+          size='default'
+          className='md:text-md h-full w-full text-xs'
+          asChild
+        >
+          <a href='https://comendrun.com'>My Website</a>
         </Button>
       </div>
     </LandingPageSection>
