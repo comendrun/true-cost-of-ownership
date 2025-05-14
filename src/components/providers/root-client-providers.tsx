3e31ea0de0e76c615f149540c71afebb9b784264
comendrun <kamran.rouhani@outlook.com>
2025-05-14 02:56:42 +0200

    - Adding framer motion animation for my pages

--- src/components/providers/root-client-providers.tsx
+++ src/components/providers/root-client-providers.tsx
@@ -3,23 +3,36 @@ import { ThemeProvider } from '@/components/ui/providers/theme-provider'
 import { Toaster } from '@/components/ui/sonner'
 import { ReactNode } from 'react'
 import { HeroUIProvider } from '@heroui/react'
+import { AnimatePresence, motion } from 'framer-motion'
+import { usePathname } from 'next/navigation'
 
 export default function RootLayoutClientProviders({
-                                                    children
-                                                  }: {
+  children
+}: {
   children: ReactNode
 }) {
+  const pathname = usePathname()
   return (
     <>
       <HeroUIProvider>
-
         <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
+          attribute='class'
+          defaultTheme='system'
           enableSystem
           // disableTransitionOnChange
         >
-          {children}
+          <AnimatePresence mode='wait' initial={false}>
+            <motion.div
+              key={pathname}
+              initial={{ opacity: 0, y: 8 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -8 }}
+              transition={{ duration: 0.1, ease: 'easeOut' }}
+              className=''
+            >
+              {children}
+            </motion.div>
+          </AnimatePresence>
         </ThemeProvider>
         <Toaster />
       </HeroUIProvider>
